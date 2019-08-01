169d7721550aec03b17c782ce21af5ee0bceb418
bray <bennettlray@gmail.com>
2019-08-01 11:27:14 -0500

    Moved input updates to InputStateManager.

--- WorkWatch.Console/WorkWatch.Console/InputStateManager.cs
+++ WorkWatch.Console/WorkWatch.Console/InputStateManager.cs
@@ -1,16 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using WorkWatch.Console.Helpers;
 
 namespace WorkWatch.Console
 {
     public class InputStateManager
     {
+        private const int UpdateFrequencyMilliseconds = 100;
+        private readonly AllInputSources _inputSources;
         private DateTime _lastInputDateTime = DateTime.MinValue;
-        public event EventHandler<EventArgs> StateUpdated;
-        public int MaxElapsedTime { get; set; }
 
+        public InputStateManager(int maxElapsedTimeMilliseconds)
+        {
+            _maxElapsedTimeMilliseconds = maxElapsedTimeMilliseconds;
+            _inputSources = new AllInputSources();
+            var threadTimer = new Timer(UpdateFrequencyMilliseconds);
+            threadTimer.Elapsed += UpdateState;
+            threadTimer.AutoReset = true;
+            threadTimer.Enabled = true;
+        }
+
+        private readonly int _maxElapsedTimeMilliseconds;
+        public event EventHandler<DateTime> StateUpdated;
+
+        private void UpdateState(object sender, ElapsedEventArgs e)
+        {
+            var previousInputDateTime = _lastInputDateTime;
+            _lastInputDateTime = _inputSources.GetLastInputTime();
+
+            if (previousInputDateTime.AddMilliseconds(_maxElapsedTimeMilliseconds) < _lastInputDateTime)
+            {
+                StateUpdated?.Invoke(this, _lastInputDateTime);
+            }
+        }
     }
 }
--- WorkWatch.Console/WorkWatch.Console/Program.cs
+++ WorkWatch.Console/WorkWatch.Console/Program.cs
@@ -1,112 +1,24 @@
 ﻿using System;
-using System.Threading;
-using System.Timers;
-using System.Windows.Threading;
-using WorkWatch.Console.Helpers;
-using Timer = System.Timers.Timer;
 
 namespace WorkWatch.Console
 {
     internal class Program
     {
-        private static DateTime? _lastMouseInput;
-        private static DateTime? _lastKeyboardInput;
-        private static DateTime _lastInput = DateTime.MinValue;
-
-        private static MouseInput mouseInput;
-        private static KeyboardInput keyboardInput;
-        private static Timer threadTimer;
-        private static AllInputSources allInputSources;
-
         private static void Main(string[] args)
         {
             System.Console.WriteLine("Listening started");
 
-            //var workThread = new Thread(Run);
-            //workThread.Start();
-
-            allInputSources = new AllInputSources();
-            threadTimer = new System.Timers.Timer(100);
-            threadTimer.Elapsed += ThreadTimerOnElapsed;
-            threadTimer.AutoReset = true;
-            threadTimer.Enabled = true;
-
+            var inputStateManager = new InputStateManager(500);
+            inputStateManager.StateUpdated += OnStateUpdated;
             do
             {
                 // spin
             } while (System.Console.ReadKey(true).Key != ConsoleKey.Escape);
-
-            threadTimer.Dispose();
-            mouseInput.Dispose();
-            keyboardInput.Dispose();
-
-            //workThread.Abort();
         }
 
-        private static void Run()
+        private static void OnStateUpdated(object sender, DateTime lastInputTime)
         {
-            System.Console.WriteLine("Thread running");
-
-            //mouseInput = new MouseInput();
-            //keyboardInput = new KeyboardInput();
-            threadTimer = new Timer(100);
-
-            allInputSources = new AllInputSources();
-            //mouseInput.MouseMoved += OnMouseInput;
-            //keyboardInput.KeyBoardKeyPressed += OnKeyboardInput;
-
-            threadTimer.Elapsed += ThreadTimerOnElapsed;
-            threadTimer.AutoReset = true;
-            threadTimer.Enabled = true;
-
-            System.Console.WriteLine("Thread still running");
-
-            //using (var )
-            //using (var )
-            //using (var)
-            //{
-
-
-            //    do
-            //    {
-
-            //    } while (true);
-
-            //    //do
-            //    //{
-            //    //    // spin
-            //    //} while (System.Console.ReadKey(true).Key != ConsoleKey.Escape);
-            //}
+            System.Console.Write($"\rLast Input: {lastInputTime:hh:mm:ss fff}");
         }
-
-        private static void ThreadTimerOnElapsed(object sender, ElapsedEventArgs e)
-        {
-            var newInputTime = allInputSources.GetLastInputTime();
-            _lastInput = newInputTime > _lastInput ? newInputTime : _lastInput;
-            PrintOutput();
-        }
-
-        private static void PrintOutput()
-        {
-            System.Console.Write("\r");
-            System.Console.Write($"Last Input: {_lastInput.ToLongTimeString()}");
-            System.Console.Write($" Mouse: {_lastMouseInput?.ToLongTimeString() ?? "Never"}");
-            System.Console.Write($" Keyboard: {_lastKeyboardInput?.ToLongTimeString() ?? "Never"}");
-
-        }
-
-        //private static void OnMouseInput(object sender, EventArgs e)
-        //{
-        //    System.Console.WriteLine("Mouse input");
-        //    _lastMouseInput = DateTime.Now;
-        //    //PrintOutput();
-        //}
-
-        //private static void OnKeyboardInput(object sender, EventArgs e)
-        //{
-        //    System.Console.WriteLine("Keyboard input");
-        //    _lastKeyboardInput = DateTime.Now;
-        //    //PrintOutput();
-        //}
     }
 }
